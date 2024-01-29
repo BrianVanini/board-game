d14be6c0c823124257413fe1c88250f331a54146
BrianVanini <bav2250@rit.edu>
2024-01-28 19:57:29 -0500

    Hovering selection finally works!

--- board game/Assets/Scripts/Board.cs
+++ board game/Assets/Scripts/Board.cs
@@ -4,57 +4,74 @@ public class Board : MonoBehaviour{
     [Header("Art")]
     [SerializeField] private Material tileMaterial;
     [SerializeField] private float tileSize = 1.0f;
-    [SerializeField] private float yOffSet = 0.2f;
+
+    // [SerializeField] private float yOffSet = 0.2f;
+    // [SerializeField] private Vector3 boardCenter = Vector3.zero;
 
     // Logic
-    private const int TILE_COUNT_X = 15;
-    private const int TILE_COUNT_Y = 15;
+    private const int TILE_COUNT_X = 9;
+    private const int TILE_COUNT_Y = 9;
     private GameObject[,] tiles;
     private Camera currentCamera;
     private Vector2Int currentHover;
+    // private Vector3 bounds;
 
     private void Awake(){
+        // Find the camera in the scene
+        currentCamera = Camera.main;
+
+        // If not found, you can try to find it by tag
+        if (currentCamera == null){
+            currentCamera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+        }
+
+        // If still not found, you might want to handle this case accordingly
+        if (currentCamera == null)
+        {
+            Debug.LogError("Main camera not found in the scene.");
+        }
         GenerateGrid(tileSize, TILE_COUNT_X, TILE_COUNT_Y);
     }
 
     private void Update(){
-        if (!currentCamera){
-            currentCamera = Camera.main;
-            return;
-        }
-
         RaycastHit info;
         Ray ray = currentCamera.ScreenPointToRay(Input.mousePosition);
-        if(Physics.Raycast(ray, out info, 100, LayerMask.GetMask("Tile"))){
-            // Get index at tile hit
-            Vector2Int hitPos = LookupTileIndex(info.transform.gameObject);
+        if (Physics.Raycast(ray, out info, 100, LayerMask.GetMask("Tile", "Hover"))){
+            Debug.Log("If statement was true.");
+            // Get the indexes of the tile i've hit
+            Vector2Int hitPosition = LookupTileIndex(info.transform.gameObject);
 
-            // If hovering tile after not hovering
-            if(currentHover == -Vector2Int.one){
-                currentHover = hitPos;
-                currentHover = hitPos;
-                tiles[hitPos.x, hitPos.y].layer = LayerMask.NameToLayer("Hover");
-            }
-            
-            // If already hovering a tile, change tile
-            if(currentHover != hitPos){
-                tiles[currentHover.x, currentHover.y].layer = LayerMask.NameToLayer("Tile");
-                currentHover = hitPos;
-                tiles[hitPos.x, hitPos.y].layer = LayerMask.NameToLayer("Hover");
-            }
-        }
-        else{
-            if(currentHover != -Vector2Int.one){
+            Debug.DrawRay(currentCamera.transform.position, ray.direction * 100, Color.green);
+
+            // If we're hovering a tile after not hovering any tiles
+            if (currentHover == -Vector2Int.one)
+            {
                 tiles[currentHover.x, currentHover.y].layer = LayerMask.NameToLayer("Tile");
                 currentHover = -Vector2Int.one;
+            }
+
+            // If we were already hovering a tile, change the previous one
+            if (currentHover != hitPosition)
+            {
+                tiles[currentHover.x, currentHover.y].layer = LayerMask.NameToLayer("Tile");
+                currentHover = hitPosition;
+                tiles[hitPosition.x, hitPosition.y].layer = LayerMask.NameToLayer("Hover");
+            }
+            else
+            {
+                if (currentHover != -Vector2Int.one)
+                {
+                    currentHover = hitPosition;
+                    tiles[hitPosition.x, hitPosition.y].layer = LayerMask.NameToLayer("Hover");
+                }
             }
         }
     }
 
     // Generate board
     private void GenerateGrid(float tileSize, int tileCountX, int tileCountY){
-        yOffSet += transform.position.y;
-        bounds = new Vector3(tileCountX/2 * tileSize, 0, tileCountX/2 * tileSize + boardCenter = 0;)
+        // yOffSet += transform.position.y;
+        // bounds = new Vector3(tileCountX/2 * tileSize, 0, tileCountX/2 * tileSize) + boardCenter;
         tiles = new GameObject[tileCountX, tileCountY];
         for (int x = 0; x < tileCountX; x++)
         {
@@ -66,18 +83,18 @@ public class Board : MonoBehaviour{
     }
 
     private GameObject GenerateTile(float tileSize, int x, int y){
-        GameObject tileObject = new GameObject(string.Format("X:{0}, Y:{1}", x, y));
+        GameObject tileObject = new(string.Format("X:{0}, Y:{1}", x, y));
         tileObject.transform.parent = transform;
 
-        Mesh mesh = new Mesh();
+        Mesh mesh = new();
         tileObject.AddComponent<MeshFilter>().mesh = mesh;
         tileObject.AddComponent<MeshRenderer>().material = tileMaterial;
 
         Vector3[] vertices = new Vector3[4];
-        vertices[0] = new Vector3(x * tileSize, yOffSet, y * tileSize) - bounds;
-        vertices[1] = new Vector3(x * tileSize, yOffSet, (y + 1) * tileSize) - bounds;
-        vertices[2] = new Vector3((x + 1) * tileSize, yOffSet, y * tileSize) - bounds;
-        vertices[3] = new Vector3((x + 1) * tileSize, yOffSet, (y + 1) * tileSize) - bounds;
+        vertices[0] = new Vector3(x * tileSize, 0, y * tileSize);
+        vertices[1] = new Vector3(x * tileSize, 0, (y + 1) * tileSize);
+        vertices[2] = new Vector3((x + 1) * tileSize, 0, y * tileSize);
+        vertices[3] = new Vector3((x + 1) * tileSize, 0, (y + 1) * tileSize);
 
         int[] tris = new int[] { 0, 1, 2, 1, 3, 2 };
 
